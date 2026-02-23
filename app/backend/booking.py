46c2b8eed5841aa49d37ebf994deb8ade044e2d9
UnoriginalUser15 <mrwillowmoody@gmail.com>
2026-02-23 10:18:49 +0000

    initial commit

---
+++ app/backend/booking.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Session
+from database import engine
+from models import HotelBooking, ZooBooking
+
+
+# used to create a new booking for the hotel
+def hotel():
+    pass
+
+
+# used to create a new booking for the zoo
+def zoo():
+    pass
