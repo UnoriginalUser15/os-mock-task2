eada1a54a80cff7ac52c6f45eb59ce95c9a9964c
UnoriginalUser15 <mrwillowmoody@gmail.com>
2026-02-25 12:49:15 +0000

    made database enforce froeign keys

--- app/backend/database.py
+++ app/backend/database.py
@@ -1,4 +1,7 @@
 import sqlalchemy
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+import sqlite3
 import os
 from models import Base, HotelBooking, RoomTable, UserTable, ZooBooking
 
@@ -10,6 +13,13 @@ __all__ = [
     "RoomTable",
     "UserTable"
 ]
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, sqlite3.Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
 
 # gets the absolute directory of backend
 path = os.path.dirname(__file__)
